7340aaa33b26b82f844c20b3078321c4f0ddde5a
Lucas Francelino <francelino25lucas@gmail.com>
2025-09-30 11:41:46 -0300

    Add email notification functionality using AWS SNS and update environment variables in docker-compose

--- src/dashboard/notificacoes/enviar.py
+++ src/dashboard/notificacoes/enviar.py
@@ -1,7 +1,19 @@
 import streamlit as st
 from src.notificacoes.email import enviar_email
+import os
+
+SNS_TOPIC_ARN = os.environ.get('SNS_TOPIC_ARN')
+SNS_REGION = os.environ.get('SNS_REGION')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
 
 def enviar_email_teste():
+
+    st.write(SNS_TOPIC_ARN)
+    st.write(SNS_REGION)
+    st.write(AWS_ACCESS_KEY_ID)
+    st.write(AWS_SECRET_ACCESS_KEY)
 
     assunto = st.text_input(label="Assunto", value="Manutenção Programada")
     mensagem = st.text_area(label="Mensagem", value="Prezado usuário, informamos que nossa Inteligência Artificial detectou que é necessário realizar uma manutenção programada no equipamento X. A manutenção ocorrerá no dia DD/MM/YYYY, das HH:MM as HH:MM (horário de Brasília). Pedimos desculpas por qualquer inconveniente e agradecemos pela compreensão.")
--- src/notificacoes/email.py
+++ src/notificacoes/email.py
@@ -1,21 +1,12 @@
 import boto3
 import os
 
-SNS_TOPIC_ARN = os.environ['SNS_TOPIC_ARN']
-SNS_REGION = os.environ['SNS_REGION']
+SNS_TOPIC_ARN = os.environ.get('SNS_TOPIC_ARN')
+SNS_REGION = os.environ.get('SNS_REGION')
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
-# Cria cliente do SNS, usando credenciais se fornecidas
-if AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY:
-    sns = boto3.client(
-        "sns",
-        region_name=SNS_REGION,
-        aws_access_key_id=AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=AWS_SECRET_ACCESS_KEY
-    )
-else:
-    sns = boto3.client("sns", region_name=SNS_REGION)
+sns = boto3.client("sns", region_name=SNS_REGION)
 
 def enviar_email(assunto, mensagem):
     response = sns.publish(
